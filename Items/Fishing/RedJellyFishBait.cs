fdb06a48dff9785d6df3c490753093aa9369320f
Yagoi23 <82478297+Yagoi23@users.noreply.github.com>
2022-07-05 10:42:23 +1200

    Update RedJellyFishBait.cs

--- Items/Fishing/RedJellyFishBait.cs
+++ Items/Fishing/RedJellyFishBait.cs
@@ -1,1 +1,32 @@
-﻿
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace YagoisCalmityAdditions.Items.Fishing
+{
+    public class RedJellyFishBait : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            //SacrificeTotal(1);
+            DisplayName.SetDefault("Red Jellyfish Bait");
+            Tooltip.SetDefault("Attracts Jellyfish");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 28;
+            Item.maxStack = 20;
+            Item.rare = ItemRarityID.Red;       
+            Item.bait = 100;
+            Item.useStyle = 1;
+            Item.autoReuse = true;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.consumable = true;
+            Item.noUseGraphic = true;
+        }
+    }
+}
